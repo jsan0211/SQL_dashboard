3110c4d322db1b835fbd82f523ba2d6d96ea9853
jsan0211 <john.santiago02211992@gmail.com>
2025-06-16 11:49:46 -0500

    trying to set up the Django routing pipeline

---
+++ live_dashboard/dashboard/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Hello from your Dashboard App!</h1>")
+
+# Create your views here.
